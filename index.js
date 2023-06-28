b5477ab3efea46b11f658d9492bef8503bce075c
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-06-27 22:36:52 -0500

    FIX - Ajuste de diseño.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 var loading = document.getElementById("loading");
 var home = document.getElementById("home");
+var projects = document.getElementById("projects");
 
 var isLoading = true;
 
 
 if (isLoading) {
     home.style.display = 'none';
+    projects.style.display = 'none';
     getData();
 }
 
@@ -17,7 +19,6 @@ function getData() {
         .then(res => res.json())
         .then(res => setInfoHome(res))
         .catch( err => console.error(err));
-        
     } catch (error) {
         throw error;
     }
@@ -25,6 +26,8 @@ function getData() {
 
 
 function setInfoHome(res) {
+
+    setDataProjects(res);
 
     var imgProfile = document.getElementById('img_profile');
     var name = document.getElementById('name');
@@ -37,4 +40,31 @@ function setInfoHome(res) {
     isLoading = false;
     loading.style.display = 'none';
     home.style.display = 'block';
+    projects.style.display = 'block';
+    console.log(res);
+}
+
+
+function setDataProjects(res) {
+    var general_projects = document.getElementById("general_projects");
+
+    var mobile = [];
+    var frontend = [];
+
+    res.projects.forEach(projects => {
+        switch (projects.id) {
+            case 1:
+                mobile = projects.projects;
+                break;
+            case 2:
+                frontend = projects.projects;
+                break;
+        }
+    });
+
+    console.log(mobile);
+    console.log(frontend);
+
+
+
 }
